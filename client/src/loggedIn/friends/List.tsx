db6785e2d84320845b7953569d51ac0401198758
Aragor70 <mikis3@o2.pl>
2021-01-24 02:22:03 +0000

    friend search list by name

--- client/src/loggedIn/friends/List.tsx
+++ client/src/loggedIn/friends/List.tsx
@@ -1,15 +1,31 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 
 
 
 
 const List = ({ pageTitle, getOnLoad, array, Component, ifEmpty, input, setInput, friend, setEditMode, editMode, setEditFriend, editFriend, history, socket }: any) => {
 
+    const [searchValue, setSearchValue] = useState('')
+    const [searched, setSearched] = useState<any>([])
+
     useEffect(() => {
         getOnLoad()
-    }, [getOnLoad])
-    console.log(array)
 
+        return () => {
+            setSearched([])
+            setSearchValue('')
+            setInput(false)
+        }
+    }, [getOnLoad])
+    
+    
+    useEffect(() => {
+        if (searchValue.length > 0) {
+        setSearched(array.filter((person: any) => person.name.toLowerCase().includes(searchValue.toLowerCase())))
+        }
+        
+    }, [searchValue])
+    
     return (
         <Fragment>
             <div className="friends-header">
@@ -18,12 +34,12 @@ const List = ({ pageTitle, getOnLoad, array, Component, ifEmpty, input, setInput
             <div className="search-row">
                 <button onClick={e=>setInput(!input)}>search</button>
                 {
-                    input && <input type="text" />
+                    input && <input type="text" value={searchValue} onChange={e=> setSearchValue(e.target.value)} />
                 }
                 
             </div>
             {
-                array.length > 0 ? array.map((person: any) => <Component key={person._id} recipient={person} invite={person} setEditMode={setEditMode} editMode={editMode} sentInvites={friend.sentInvites} history={history} setEditFriend={setEditFriend} editFriend={editFriend} socket={socket} />) :  <div className="friends-row"><span className="empty">{ifEmpty}</span></div>
+                searchValue.length > 0 ? searched.map((person: any) => <Component key={person._id} recipient={person} invite={person} setEditMode={setEditMode} editMode={editMode} sentInvites={friend.sentInvites} history={history} setEditFriend={setEditFriend} editFriend={editFriend} socket={socket} />) : array.length > 0 ? array.map((person: any) => <Component key={person._id} recipient={person} invite={person} setEditMode={setEditMode} editMode={editMode} sentInvites={friend.sentInvites} history={history} setEditFriend={setEditFriend} editFriend={editFriend} socket={socket} />) :  <div className="friends-row"><span className="empty">{ifEmpty}</span></div>
             }
         </Fragment>
     );
